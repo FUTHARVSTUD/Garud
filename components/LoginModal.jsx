27d77bcb055459ad8f66ac636a88a3da2dd7eed3
subbbuwu <nsubbu2004@gmail.com>
2024-04-08 18:15:50 +0530

    added push

--- components/LoginModal.jsx
+++ components/LoginModal.jsx
@@ -1,14 +1,20 @@
 import { useLoginModalState } from '@/store/store'
 import React from 'react'
 
+
 const LoginModal = () => {
     const setIsOpen = useLoginModalState((state) => state.updateOpen)
+    
+    const handleAuthClick = () => {
+        window.location.href = "/dashboard";
+      };
+
   return (
     <div className="z-[999] overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0  flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div className="relative p-4 w-full max-w-2xl max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-3xl w-full text-center font-semibold text-gray-900 dark:text-white">
+                <h3 className="text-3xl cursor-pointer w-full text-center font-semibold text-gray-900 dark:text-white" >
                     Login  
                 </h3>
                 <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={
@@ -20,8 +26,8 @@ const LoginModal = () => {
                 </button>
             </div>
 
-            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                Google sign in goes here then redirect them to "/dashboard"
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"  onClick={handleAuthClick}>
+                Click me to go to dashboard
             </div>
         </div>
     </div>
